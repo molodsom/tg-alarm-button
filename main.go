e5e497e13adf04484b87c9f5ed64f2d2b4290ffc
check1st <victor.ryzhikov@gmail.com>
2023-01-10 16:31:29 +0400

    Display the response body if an error occurs

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -20,19 +21,22 @@ func main() {
 			getHostname(), getUsername(), getIPs(),
 		),
 	})
-	responseBody := bytes.NewBuffer(postBody)
-	_, err := http.Post(url, "application/json", responseBody)
+	r, err := http.Post(url, "application/json", bytes.NewBuffer(postBody))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Printf("OK")
+	if r.StatusCode != 200 {
+		b, _ := io.ReadAll(r.Body)
+		log.Fatalln(string(b))
+	}
+	log.Println("OK")
 }
 
 func getUsername() string {
 	u, err := user.Current()
 	if err != nil {
-		log.Fatalf(err.Error())
-		return ""
+		log.Println(err.Error())
+		return "Unknown username"
 	}
 	return u.Username
 }
@@ -49,7 +53,7 @@ func getIPs() string {
 	var ips []string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return ""
+		return "Unknown address"
 	}
 	for _, addr := range addrs {
 		ips = append(ips, addr.String())
